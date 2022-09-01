b66bb1cc079191762fc0267ec864d18dd2b2e4e0
Ghost <mbflug@gmail.com>
2022-09-01 15:27:11 -0600

    updating files

---
+++ library/Intern.js
@@ -0,0 +1,16 @@
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+  constructor(name, id, email, schoolName) {
+    super(name, id, email);
+    this.schoolName = schoolName;
+  }
+  getschoolName() {
+    return this.schoolName;
+  }
+  getRole() {
+    return "Intern";
+  }
+}
+
+module.exports = Intern;
